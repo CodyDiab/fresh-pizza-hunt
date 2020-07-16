2b9b608b91985677693b0b6b2bae9e151bc68c3c
Diab <emailcodydiab@gmail.com>
2020-07-16 07:18:57 -0500

    just trying to get a fresh repo

---
+++ fresh-pizza-hunt/models/Comment.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReplySchema = new Schema(
+   {
+      // set custom id to avoid confusion with parent comment _id
+      replyId: {
+         type: Schema.Types.ObjectId,
+         default: () => new Types.ObjectId()
+      },
+      replyBody: {
+         type: String,
+         required: true
+      },
+      writtenBy: {
+         type: String,
+         required: true,
+         trim: true
+      },
+      createdAt: {
+         type: Date,
+         default: Date.now,
+         get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
+   },
+   {
+      toJSON: {
+         getters: true
+      }
+   }
+);
+
+const CommentSchema = new Schema(
+   {
+      writtenBy: {
+         type: String,
+         required: true,
+         trim: true
+      },
+      commentBody: {
+         type: String,
+         required: true,
+         trim: true
+      },
+      createdAt: {
+         type: Date,
+         default: Date.now,
+         get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      },
+      replies: [ReplySchema]
+   },
+   {
+      toJSON: {
+         virtuals: true,
+         getters: true
+      }
+   }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+   return this.replies.length;
+});
+
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
